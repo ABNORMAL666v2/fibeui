07459e14661ff17edb84aa63af3da1a5f6eeb2e2
Normal <101961638+ABNORMAL666v2@users.noreply.github.com>
2024-02-08 16:48:51 -0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+    tailwind.config = {
+        theme: {
+            extend: {
+                colors: {
+                    'eerie': '#1f1f1f',
+                    'jet': '#2a2a2a',
+                    'onyx': '#434343',
+                    'davy': '#5c5c5c',
+                    'gray': '#767676',
+                    'dim-gray': '#747474',
+                    'battleship': '#8d8d8d',
+                    'warship': '#979797',
+                    'cadet': '#a0a0a0',
+                    'silver': '#a9a9a9',
+                }
+            }
+        }
+    }
